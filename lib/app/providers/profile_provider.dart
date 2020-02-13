1469dfe061ecbf2320bdfc9e7f345f7735c52856
Ivan Ray Altomera <irayspacii@gmail.com>
2020-02-14 01:24:10 +0800

    feat: remove unnecessary provider

--- lib/app/providers/profile_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:nutrition_app/core/repositories/profile_repository.dart';
-
-
-class ProfileProvider with ChangeNotifier {
-  ProfileRepository _repository;
-  ProfileProvider() : _repository = ProfileRepository();
-}
